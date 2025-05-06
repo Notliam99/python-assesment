1fda3b9d85c591b5a7fd9d8376eff20a328b4d2b
Liam Tietjens <l.tietjens@hotmail.com>
2025-05-07 10:24:30 +1200

    working api framework absalote fire

--- python-assesment/main.py
+++ python-assesment/main.py
@@ -1,2 +1,19 @@
+from starlette.applications import Starlette
+from hypercorn import Config
+from hypercorn.asyncio import serve
+from lib.api import API
+import asyncio
+
+# NOTE: API class
+api = API()
+
+# NOTE: api.routes list of Routes Defined by decorated functions
+app = Starlette(routes=api.routes)
+
 if __name__ == "__main__":
-    print("helloWorld")
+    config = Config()
+    # NOTE: (0.0.0.0) declears any connection from any ip is ok and (:8000) defines the server port
+    config.bind = ["0.0.0.0:8000"]
+
+    # NOTE: async runtime excutes the async server
+    asyncio.run(serve(app, config))
